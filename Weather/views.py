83a3c50b17aba415409a20a75a5782577591c52c
TimofeyKupriyanov <timofey.kupriyanov@yandex.by>
2025-06-17 16:18:41 +0300

    first commit

---
+++ Weather/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from django.contrib import messages
+from django.conf import settings
+from .models import City
+from .forms import CityForm
+import requests
+
+
+def index(request):
+    key = settings.OPENWEATHER_API_KEY
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + key
+    all_cities = []
+    form = CityForm()
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            city_name = form.cleaned_data['name']
+
+            try:
+                response = requests.get(url.format(city_name))
+                response.raise_for_status()
+                weather_data = response.json()
+
+                if weather_data['cod'] == 200:
+                    form.save()
+                    messages.success(request, 'Город успешно добавлен')
+                else:
+                    messages.error(request, 'Город не найден')
+
+            except requests.exceptions.RequestException as e:
+                messages.error(request, f'Ошибка соединения: {str(e)}')
+            except KeyError:
+                messages.error(request, 'Некорректный ответ от сервера погоды')
+
+    cities = City.objects.all()
+
+    for city in cities:
+        try:
+            res = requests.get(url.format(city.name)).json()
+            if res['cod'] == 200:
+                city_info = {
+                    "city": city.name,
+                    "temperature": round(res["main"]["temp"], 1),
+                    "icon": res["weather"][0]["icon"],
+                }
+                all_cities.append(city_info)
+        except:
+            continue
+
+    context = {
+        "all_info": all_cities,
+        "form": form
+    }
+
+    return render(request, "index.html", context)
